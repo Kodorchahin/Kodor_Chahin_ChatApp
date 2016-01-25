566c1d55ea4cf2f20902a33115e1cc3a17753d6e
Kodor Chahin <kodorchahin@gmail.com>
2016-01-25 03:19:45 -0500

    Create logout.php

---
+++ logout.php
@@ -0,0 +1,16 @@
+<?php
+error_reporting(E_ALL & ~E_NOTICE);
+session_start();
+session_destroy();
+?>
+
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Logout</title>
+	</head>
+	<body style="background-color:#e2e7e9;">
+	<h2><a href = "index.php"> Return to Login </a></h2>
+
+	</body>
+</html>
